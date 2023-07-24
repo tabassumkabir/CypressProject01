f1b0840c38932e31ef02b9ec822b0636dd9033a4
Tabassum Kabir <tabassumkabir123@gmail.com>
2023-07-24 20:39:35 +0600

    dropdown without select

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -2,6 +2,7 @@ describe('Dropdowns',()=>{
 
 
     const zohoUrl="https://www.zoho.com/commerce/free-demo.html"
+    const dummyUrl="https://www.dummyticket.com/dummy-ticket-for-visa-application/"
     it('dropdown with select tag',()=>{
 
     
@@ -11,4 +12,19 @@ describe('Dropdowns',()=>{
         cy.get("#zcf_address_country").select('Germany')
 
     })
+
+    it.only('dropdown without select tag',()=>{
+
+    
+        cy.visit(dummyUrl)
+
+        //select the country dropdown
+        cy.get('#select2-billing_country-container').click()
+
+        cy.get(".select2-search__field").type('Germany')
+
+    })
+
+
+
 })
